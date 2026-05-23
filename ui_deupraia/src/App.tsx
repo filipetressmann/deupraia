2091c7596e618b0ab6928ceee695cdc8701a0831
filipe tressmann <filipetressmann2@gmail.com>
2026-05-23 19:14:00 -0300

    Add helmet and page metadata

--- ui_deupraia/src/App.tsx
+++ ui_deupraia/src/App.tsx
@@ -6,6 +6,7 @@ import BeachMap from './BeachMap';
 import { type LatLngExpression } from 'leaflet';
 import SearchBar from './SearchBar';
 import './styling/App.css';
+import { Helmet } from 'react-helmet-async'
 
 function App() {
 
@@ -24,18 +25,54 @@ function App() {
   }, []);
 
   return (
-    <div className="app-wrapper">
-      <SearchBar 
-        beaches={points} 
-        setCenter={setCenter}
-        setSelectedBeach={setSelectedBeach}
-      />
-      <BeachMap 
-        points={points} 
-        center={center}
-        selectedBeach={selectedBeach}
-      />
-    </div>
+    <>
+      <Helmet>
+        <title>
+          Brazil Beach Water Quality Map
+        </title>
+
+        <meta
+          name="description"
+          content="
+            Mapa de balneabilidade das praias do Brasil. Este mapa possui dados
+            das praias do Rio de Janeiro, São Paulo, Santa Catarina e Espirito Santo."
+        />
+
+        <meta
+          name="keywords"
+          content="
+            qualidade das aguas das praias do brasil, 
+            praias brasileiras, balneabilidade das praias, 
+            praias limpas"
+        />
+
+        <meta
+          property="og:title"
+          content="Mapa de Balneabilidade das Praias do Brasil"
+        />
+
+        <meta
+          property="og:description"
+          content="Consulte a qualidade da água e as condições de banho das praias brasileiras."
+        />
+        <meta
+          property="og:type"
+          content="website"
+        />
+      </Helmet>
+      <main className="app-wrapper">
+        <SearchBar 
+          beaches={points} 
+          setCenter={setCenter}
+          setSelectedBeach={setSelectedBeach}
+        />
+        <BeachMap 
+          points={points} 
+          center={center}
+          selectedBeach={selectedBeach}
+        />
+      </main>
+    </>
   )
 }
 
